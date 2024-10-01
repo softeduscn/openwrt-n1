1f01f8664f252110fc56623a4e3b93975f83e4a1
softeduscn <softeduscn@gmail.com>
2024-10-01 22:15:02 +0800

    Update

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,13 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i s/"libpcre"/"libpcre2"/ package/feeds/telephony/freeswitch/Makefile
 sed -i s/'default LIBCURL_WOLFSSL'/'default LIBCURL_OPENSSL'/ feeds/packages/net/curl/Config.in
-rm -rf feeds/packages/net/v2raya
-rm -rf feeds/packages/net/trojan-go
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/lang/golang
-mv package/lean/golang feeds/packages/lang
+[ -d package/lean/v2raya ] && rm -rf feeds/packages/net/v2raya
+[ -d package/lean/trojan-go ] && rm -rf feeds/packages/net/trojan-go
+[ -d package/lean/xray-core ] && rm -rf feeds/packages/net/xray-core
+[ -d package/lean/smartdns ] && rm -rf feeds/packages/net/smartdns
+[ -d package/lean/luci-app-smartdns ] && rm -rf feeds/luci/applications/luci-app-smartdns
+[ -d package/lean/v2ray-geodata ] && rm -rf feeds/packages/net/v2ray-geodata
+if [ -d package/lean/golang ]; then
+	rm -rf feeds/packages/lang/golang
+	mv package/lean/golang feeds/packages/lang
+fi
